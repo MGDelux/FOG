8a70e34900b39b583540ac7eb8ef73d576833dea
Tenz331 <61831296+Tenz331@users.noreply.github.com>
2020-11-30 15:23:27 +0100

    Splittet forespørgelse op

--- src/main/java/web/pages/foresporgelse.java
+++ src/main/java/web/pages/foresporgelse.java
@@ -8,26 +8,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.sql.SQLException;
 
 /**
- * CREATED BY mathi @ 23-11-2020 - 14:20
+ * CREATED BY Emil @ 30-11-2020 - 14:26
  **/
-@WebServlet({"/carport", "/carport/*"})public class Carport extends BaseServlet {
+@WebServlet({"/foresporgelse", "/foresporgelse/*"})public class foresporgelse extends BaseServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        render("/WEB-INF/pages/carport.jsp", resp, req);
+        render("/WEB-INF/pages/foresporgelse.jsp", resp, req);
 
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
-        if (req.getParameter("placeOrder") != null) {
-            getServletContext().getRequestDispatcher("/WEB-INF/pages/foresporgelse.jsp").forward(req, resp);
-
-        }
-
     }
 }
